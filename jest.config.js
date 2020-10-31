3b60592aa657ad94a38d293a83b317664f3b5e91
JamilsonMello <jamilsonmelow@gmail.com>
2020-10-31 19:19:23 -0300

    add config jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,26 @@
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/controllers/**/*.ts'
+  ],
   preset: 'ts-jest',
+  clearMocks: true,
   testEnvironment: 'node',
+  transform: {
+    '.+\\.spec.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)':'<rootDir>/src/$1',
+  },
+  coverageReporters: [
+    "text-summary",
+     "lcov",
+   ],
+  transformIgnorePatterns: [
+      "/node_modules/"
+  ],
+  testMatch: [
+    '<rootDir>/__tests__/**/*.spec.ts',
+  ]
 };
