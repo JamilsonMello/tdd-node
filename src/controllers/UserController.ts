eb142d52e6c30b64a600d342707388e92d90e979
JamilsonMello <jamilsonmelow@gmail.com>
2020-11-02 23:02:33 -0300

    create user controller

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm'
+
+import UserRepository from '../Repository/UserRepository'
+
+class UserController {
+  public async store(request: Request, response: Response): Promise<Response> {
+    const userRepository = getCustomRepository(UserRepository);
+    const { name, email, password } = request.body;
+
+    const userExist = await userRepository.findOne({ where: { email } })
+
+    if (userExist) {
+      return response.status(401).json({ error: 'User already exists.' });
+    }
+
+    const user = await userRepository.store({ name, email, password });
+
+    return response.status(200).json(user);
+  }
+}
+
+export default new UserController();
