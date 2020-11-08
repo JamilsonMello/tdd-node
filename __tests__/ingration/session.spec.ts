479aa4fb1341c46253af10a071062849c031e1d9
JamilsonMello <jamilsonmelow@gmail.com>
2020-11-07 21:35:55 -0300

    create session test

--- __tests__/ingration/session.spec.ts
+++ __tests__/ingration/session.spec.ts
@@ -1,23 +1,46 @@
 import request from 'supertest';
+import { getCustomRepository } from 'typeorm';
 
+import connection from '@/database'
 import App from '@/app';
 import UserRepository from '@/Repository/UserRepository';
 
 describe('Authentication', () => {
-  it('Should be able authenticate with valid credentials', async () => {
+  beforeAll(async () => {
+    await connection.create();
+  });
 
-    const user = await UserRepository.store({
+  beforeEach(async () => {
+    await connection.clear();
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('Should be able authenticate with valid credentials', async () => {
+    const userRepository = getCustomRepository(UserRepository);
+
+    await userRepository.store({
       name: 'user',
       email: 'user@example.com',
       password: 'password',
-    })
+    });
 
-    // const response = await request(App).post('/sessions').send({
-    //   email: user.name,
-    //   password: user.password,
-    // })
+    const response = await request(App).post('/sessions').send({
+      email: 'user@example.com',
+      password: 'password',
+    });
 
-    expect(1+1).toBe(2)
+    expect(response.status).toBe(200);
+  });
 
-  })
-})
+  it('Should not be able create a session to non-existing user', async () => {
+    const response = await request(App).post('/sessions').send({
+      email: 'user@example.com',
+      password: 'password',
+    });
+
+    expect(response.status).toBe(401);
+  });
+});
